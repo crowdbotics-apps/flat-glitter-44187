5d56fa98ad6598ed9f6d59979988b4b1d80dee3f
Crowdbotics <team@crowdbotics.com>
2023-10-28 11:57:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-glitter-44187",
+  url: "https://flat-glitter-44187.botics.co",
+  api: "https://flat-glitter-44187.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
